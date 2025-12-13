4b26aacf945f2a6802a857da3034b87b93bc8b9d
ptyler14 <31662240+ptyler14@users.noreply.github.com>
2025-12-13 09:27:17 -0800

    Create script.js

---
+++ ask-processes/script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Ask & It Is Given Hub Loaded");
+    
+    // FUTURE: We will add Supabase connection logic here.
+    // This will allow Process #1 (Rampage) and Process #2 (Magic Box)
+    // to save data to the same database without rewriting code.
+});
